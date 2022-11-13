8e0d85cc0b3b493f12642a0c9c92e58edb453b58
Eban Sayago <eban.sayago@protonmail.com>
2022-11-13 12:55:13 -0300

    :sparkles: PWA

---
+++ README.md
@@ -0,0 +1,5 @@
+```html
+<a href="https://www.flaticon.es/iconos-gratis/fosforo" title="fosforo iconos">
+    Fosforo iconos creados por Nathania Desmonda Tjipto - Flaticon
+</a>
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
 import { rollupPluginHTML } from '@web/rollup-plugin-html'
 import resolve from '@rollup/plugin-node-resolve'
 
@@ -15,15 +17,14 @@ export default defineConfig(({ mode }) => {
                 input: 'index.html',
                 plugins: [
                     rollupPluginHTML({
-                        // injectServiceWorker: true,
-                        // serviceWorkerPath: 'sw.js',
+                        injectServiceWorker: true,
+                        serviceWorkerPath: 'sw.js',
                         transformHtml: html => {
                             return html
-                                // .replace('index.scss', 'style.css')
-                                // .replace('</head>' , `
-                                //   <link rel="manifest" href="manifest.webmanifest">
-                                // </head>
-                                // `)
+                                .replace('</head>' , `
+                                  <link rel="manifest" href="manifest.webmanifest">
+                                </head>
+                                `)
                                 .replace('public/', '')
                         },
                         minify: true
@@ -34,6 +35,26 @@ export default defineConfig(({ mode }) => {
             minify: 'esbuild',
             cssCodeSplit: false,
         },
+        plugins: [
+            VitePWA({
+                manifest: {
+                    name:'Contador de truco',
+                    short_name: 'Truco',
+                    description: 'Contador de truco.',
+                    start_url: '/',
+                    theme_color: '#000000',
+                    icons: [
+                        {
+                            src: 'maskable_icon_x512.png',
+                            sizes: '512x512',
+                            type: 'image/png',
+                            purpose: 'any maskable'
+                        }
+                    ]
+                },
+                strategies: 'injectManifest'
+            })
+        ],
         define: {
             'process.env': { NODE_ENV: mode }
         }
